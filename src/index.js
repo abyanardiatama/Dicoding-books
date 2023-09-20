8ab5f39bd51e06b87cf92f37aa0aa32606d89ce7
abyanardiatama <abyanardiatama@students.undip.ac.id>
2023-09-20 07:07:22 +0700

    cara menggunakan API

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/*
+        jangan ubah kode di bawah ini ya!
+*/
+
+import 'regenerator-runtime';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.css';
+import main from './scripts/main';
+
+// const cors = require('cors');
+// const corsOptions ={
+//     origin:'http://localhost:8080', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+main();
